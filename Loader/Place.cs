e38a9bf2a7fa0736e39e7fcbe7e2fc951df77812
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2018-10-16 21:50:35 +0200

    Excel worker success build

---
+++ Loader/Place.cs
@@ -0,0 +1,9 @@
+﻿namespace DataModel
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public double PozitionX { get; set; }
+        public double PozitionY { get; set; }
+    }
+}
