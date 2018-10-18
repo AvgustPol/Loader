1fa2bc79267f4ac35906eb86f6cfe49575048fde
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2018-10-18 14:38:16 +0200

    Added working PMX operator

--- Loader/Place.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataModel
-{
-    public class Place
-    {
-        public int Id { get; set; }
-        public double PozitionX { get; set; }
-        public double PozitionY { get; set; }
-    }
-}
